ab28d1c45dc2bfa2abde8622e9cb17db123ab036
hacktion <matthieu.wandolski@gmail.com>
2021-01-22 08:50:28 +0100

    add settings vue and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import EmployeesList from '../components/EmployeesList.vue';
+import Settings from '../components/Settings.vue';
 import Home from '../components/Home.vue';
 
 const routes = [
   { path: '/employees', component: EmployeesList },
+  { path: '/settings', component: Settings },
   { path: '/', component: Home },
 ];
 
